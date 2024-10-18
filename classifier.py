8b46c8e32ba1cd94c067621761b8c6e504cd86c8
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2024-10-18 02:55:30 -0400

    Fixed get_data function
    Fixed a bug where the function wouldn't work if there were commas in the review

--- classifier.py
+++ classifier.py
@@ -1,13 +1,14 @@
+from language_model import preprocess_text, create_vocabulary
+
+
 def get_data(reviews: list[str], sentiment) -> list[str]:
     i = 0
-    required_list: list[str] = []
-    reviews_sentiments: list[str] = [
-        each_review.split(",")[1].strip() for each_review in reviews
-    ]
+    reviews_without_sentiments: list[str] = []
+    reviews_sentiments: list[str] = [each_review[-8:] for each_review in reviews]
     while i < len(reviews):
         if reviews_sentiments[i] == sentiment:
-            required_list.append(reviews[i].split(",")[0].strip())
+            reviews_without_sentiments.append(reviews[i][: len(reviews[i]) - 10])
             i += 1
         else:
             i += 1
-    return required_list
+    return reviews_without_sentiments
