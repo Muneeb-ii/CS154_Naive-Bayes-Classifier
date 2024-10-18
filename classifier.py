0625e9014d9fd229c846797d580f2bfef43217ac
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2024-10-18 03:49:05 -0400

    Implemented create_word_count_table function

--- classifier.py
+++ classifier.py
@@ -12,3 +12,26 @@ def get_data(reviews: list[str], sentiment) -> list[str]:
         else:
             i += 1
     return reviews_without_sentiments
+
+
+def create_word_count_table(
+    list_of_sentiment_reviews: list[str], vocabulary: set[str]
+) -> dict[str, int]:
+    preprocessed_list_of_reviews: list[str] = [
+        preprocess_text(each_review) for each_review in list_of_sentiment_reviews
+    ]
+    sentiment_word_counter: dict[str, int] = {}
+    for each_word in vocabulary:
+        for each_preprocessed_review in preprocessed_list_of_reviews:
+            if each_word in sentiment_word_counter:
+                sentiment_word_counter.update(
+                    {
+                        each_word: sentiment_word_counter.get(each_word)
+                        + each_preprocessed_review.count(each_word)
+                    }
+                )
+            else:
+                sentiment_word_counter.update(
+                    {each_word: each_preprocessed_review.count(each_word)}
+                )
+    return sentiment_word_counter
