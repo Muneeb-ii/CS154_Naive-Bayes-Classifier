6f859d3cee8a46f39c1d81a568d3058a017c1c32
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2024-10-23 13:41:23 -0400

    Final Code
    I trained my model on test_reviews. I tried two random reviews from IMBD on my model and got the correct label.

--- classifier.py
+++ classifier.py
@@ -75,3 +75,43 @@ def predict_label(
         return "Positive"
     else:
         return "Negative"
+
+
+reviews: list[str] = get_file_contents("test_reviews.csv").splitlines()[1:]
+
+list_of_positive_reviews = get_data(reviews, "positive")
+
+list_of_negative_reviews = get_data(reviews, "negative")
+
+vocabulary: set[str] = create_vocabulary(list_of_positive_reviews)
+vocabulary_negative: set[str] = create_vocabulary(list_of_negative_reviews)
+vocabulary.update(vocabulary_negative)
+
+positive_sentiment_word_count = create_word_count_table(
+    list_of_positive_reviews, vocabulary
+)
+
+negative_sentiment_word_count = create_word_count_table(
+    list_of_negative_reviews, vocabulary
+)
+
+len_positive_words = sum(positive_sentiment_word_count.values())
+prob_positive_words = calculate_likelihood_for_each_word(
+    positive_sentiment_word_count, len_positive_words
+)
+
+len_negative_words = sum(negative_sentiment_word_count.values())
+prob_negative_words = calculate_likelihood_for_each_word(
+    negative_sentiment_word_count, len_negative_words
+)
+
+positive_review_prior_probability = calculate_prior_probability(reviews, "positive")
+print(positive_review_prior_probability)
+negative_review_prior_probability = calculate_prior_probability(reviews, "negative")
+print(negative_review_prior_probability)
+
+#Testing the predict_label function
+test_review_1_IMBD: str = "Probably my all-time favorite movie, a story of selflessness, sacrifice and dedication to a noble cause, but it's not preachy or boring. It just never gets old, despite my having seen it some 15 or more times in the last 25 years. Paul Lukas' performance brings tears to my eyes, and Bette Davis, in one of her very few truly sympathetic roles, is a delight. The kids are, as grandma says, more like ""dressed-up midgets"" than children, but that only makes them more fun to watch. And the mother's slow awakening to what's happening in the world and under her own roof is believable and startling. If I had a dozen thumbs, they'd all be ""up"" for this movie." #Positive 
+test_review_2_IMBD: str = "The cast played Shakespeare.<br /><br />Shakespeare lost.<br /><br />I appreciate that this is trying to bring Shakespeare to the masses, but why ruin something so good.<br /><br />Is it because 'The Scottish Play' is my favorite Shakespeare? I do not know. What I do know is that a certain Rev Bowdler (hence bowdlerization) tried to do something similar in the Victorian era.<br /><br />In other words, you cannot improve perfection.<br /><br />I have no more to write but as I have to write at least ten lines of text (and English composition was never my forte I will just have to keep going and say that this movie, as the saying goes, just does not cut it." #Negative
+print(predict_label(test_review_1_IMBD, positive_review_prior_probability, negative_review_prior_probability, prob_positive_words, prob_negative_words ))
+print(predict_label(test_review_2_IMBD, positive_review_prior_probability, negative_review_prior_probability, prob_positive_words, prob_negative_words ))
